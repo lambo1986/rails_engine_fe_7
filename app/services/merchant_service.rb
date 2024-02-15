ed363720daa18fea2e4b482055eb3a1520400013
Nathan Lambertson <lambertones@gmail.com>
2024-02-14 20:57:01 -0600

    Change name of service variables

--- app/services/merchant_service.rb
+++ app/services/merchant_service.rb
@@ -2,8 +2,8 @@ class MerchantService# 1 # requires rails_engine app running console (https://gi
   BASE_URL = "http://localhost:3000"
 
   def all_merchants#1
-    merchant_hash = get_url("/api/v1/merchants")
-    merchant_hash[:data]
+    merchants = get_url("/api/v1/merchants")
+    merchants[:data]
   end
 
   def merchant_items(merchant_id)
