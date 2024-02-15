fa4f8738571d267a86c771a8642edd73f0128110
Nathan Lambertson <lambertones@gmail.com>
2024-02-14 21:02:47 -0600

    Add small fix

--- spec/items/show_spec.rb
+++ spec/items/show_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe "item show page", vcr: true, type: :feature do
     click_link("Item Nemo Facere")
 
     expect(current_path).to eq("/items/4")
-    expect(page).to have_content("Items")
+    expect(page).to have_content("Item")
     expect(page).to have_content("Name: Item Nemo Facere")
     expect(page).to have_content("Description: Sunt eum id eius magni consequuntur delectus veritatis. Quisquam laborum illo ut ab. Ducimus in est id voluptas autem.")
     expect(page).to have_content("Unit Price: 42.91")
